5543b84de24162ce768da46791a0ab33b68f38c9
vwxyraz <74700868+vwxyraz@users.noreply.github.com>
2021-11-07 15:05:58 +0700

    coba

---
+++ main.js
@@ -0,0 +1,27 @@
+const canvas = document.querySelector('#canvas');
+const sceneManager = new scene(canvas);
+
+bindEventListeners();
+render();
+
+function bindEventListeners() {
+    window.onresize = resizeCanvas;
+    resizeCanvas();
+}
+
+function resizeCanvas() {
+    canvas.style.width = '100%';
+    canvas.style.height = '100%';
+
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+
+    scene.onWindowResize();
+}
+
+function render(time) {
+    // convert time into seconds
+    time *= 0.001;
+    requestAnimationFrame(render);
+    scene.update(time);
+}
